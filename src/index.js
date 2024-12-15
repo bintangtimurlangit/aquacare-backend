f8df2da3c692163d279a0374e20fb4db13f3aebb
Bintang Timurlangit <btimurlangit@gmail.com>
2024-12-15 21:35:47 +0700

    fix: Change the app to run on both IPv4 and IPv6

--- src/index.js
+++ src/index.js
@@ -23,6 +23,6 @@ app.use('/api/devices', deviceRoutes);
 app.use('/api', feedingRoutes);
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
+server.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
