8820e821c6124a400cc489fdfecd8c76d9888de7
Bintang Timurlangit <btimurlangit@gmail.com>
2024-12-19 18:43:02 +0700

    chore: add GitHub workflow and configuration files

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  testEnvironment: 'node',
+  coverageDirectory: 'coverage',
+  collectCoverageFrom: [
+    'src/**/*.js',
+    '!src/index.js',
+    '!**/node_modules/**'
+  ],
+  coverageThreshold: {
+    global: {
+      statements: 80,
+      branches: 80,
+      functions: 80,
+      lines: 80
+    }
+  }
+}; 
