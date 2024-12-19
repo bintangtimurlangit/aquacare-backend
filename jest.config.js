a418e10635792afae43d94742421ff777c74f359
Bintang Timurlangit <btimurlangit@gmail.com>
2024-12-19 19:01:02 +0700

    feat: Added Babel config and updated Jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   testEnvironment: 'node',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
@@ -13,5 +13,10 @@ module.exports = {
       functions: 80,
       lines: 80
     }
-  }
+  },
+  transform: {
+    '^.+\\.js$': 'babel-jest'
+  },
+  moduleFileExtensions: ['js', 'json'],
+  testMatch: ['**/__tests__/**/*.js', '**/?(*.)+(spec|test).js']
 }; 
