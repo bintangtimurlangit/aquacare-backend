1b709433787601518c5adbd2cc408f4c2ef2abf2
Bintang Timurlangit <btimurlangit@gmail.com>
2024-12-19 18:51:57 +0700

    fix: Edit ESLint config to use new flat config format

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
+import globals from 'globals';
 
 export default [
   js.configs.recommended,
@@ -9,15 +10,15 @@ export default [
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: 'module',
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.es2022
+      }
     },
     rules: {
       'no-console': 'warn',
-      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    },
-    env: {
-      node: true,
-      es2022: true,
-      jest: true,
-    },
+      'no-unused-vars': 'off',
+    }
   },
 ]; 
